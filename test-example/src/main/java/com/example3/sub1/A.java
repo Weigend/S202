ed3225a58ca5e91ac2ced25d73fd5e277c320d89
jweigend <johannes.weigend@gmail.com>
2026-06-09 19:32:09 -0300

    Add A->C edge in example3: completes sub1<->sub2 package cycle

--- test-example/src/main/java/com/example3/sub1/A.java
+++ test-example/src/main/java/com/example3/sub1/A.java
@@ -1,13 +1,17 @@
 package com.example3.sub1;
 
+import com.example3.sub2.C;
+
 /**
  * example3 package-cycle demo.
- * A depends on B (2 edges: INSTANTIATES + CALLS), establishing A above B.
+ * A depends on B (2 edges, sub1 internal) and on C (sub1->sub2, creates package cycle).
  */
 public class A {
     private B b = new B();
+    private C c = new C();
 
     public void doA() {
         b.doB();
+        c.doC();
     }
 }
