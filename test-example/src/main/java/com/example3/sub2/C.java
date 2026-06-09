e033c76cc0f71f39efdfeed3f74923b7b46f6757
jweigend <johannes.weigend@gmail.com>
2026-06-09 19:27:32 -0300

    Remove C->D dependency in example3

--- test-example/src/main/java/com/example3/sub2/C.java
+++ test-example/src/main/java/com/example3/sub2/C.java
@@ -4,14 +4,12 @@ import com.example3.sub1.B;
 
 /**
  * example3 package-cycle demo.
- * C depends on D (2 edges: INSTANTIATES + CALLS) and on B (sub2->sub1).
+ * C depends on B (sub2->sub1).
  */
 public class C {
-    private D d = new D();
     private B b = new B();
 
     public void doC() {
-        d.doD();
         b.doB();
     }
 }
